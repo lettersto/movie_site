6a0dbf148a0af91b55f6bf8d346bf426f4269cd3
정혜령 <jhr3681@naver.com>
2022-05-20 15:39:09 +0900

    add modules to store

--- final-pjt-front/src/store/index.js
+++ final-pjt-front/src/store/index.js
@@ -1,17 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import movies from './modules/movies'
+import community from './modules/community'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    movies, community
   }
 })
