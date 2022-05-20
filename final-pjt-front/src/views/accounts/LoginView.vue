e81a8f358a2d1e4f0bfe3a72cd4765330aa6d080
정혜령 <jhr3681@naver.com>
2022-05-20 16:55:46 +0900

    simple navbar

--- final-pjt-front/src/views/accounts/LoginView.vue
+++ final-pjt-front/src/views/accounts/LoginView.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
     <h1>Login</h1>
+    <form>
+      <div>
+        <label for="username">아이디: </label>
+        <input type="text" id="username" required />
+      </div>
+      <div>
+        <label for="password">비밀번호: </label>
+        <input type="password" id="password" required />
+      </div>
+
+      <button>로그인</button>
+    </form>
   </div>
 </template>
 
--- final-pjt-front/src/views/accounts/SignupView.vue
+++ final-pjt-front/src/views/accounts/SignupView.vue
@@ -3,9 +3,22 @@
     <h1>SignUp</h1>
     <form>
       <div>
-        <label for="username"></label>
-        <input type="text" id="username" placeholder="아이디" />
+        <label for="username">아이디: </label>
+        <input type="text" id="username" />
       </div>
+      <div>
+        <label for="password1">비밀번호: </label>
+        <input type="password" id="password1" />
+      </div>
+      <div>
+        <label for="password2">비밀번호 확인: </label>
+        <input type="password" id="password2" />
+      </div>
+      <div>
+        <label for="email">이메일: </label>
+        <input type="email" id="email" />
+      </div>
+      <button>회원 가입</button>
     </form>
   </div>
 </template>
