e78963d504ee05effab2ccfef6aeee0819844d5f
정혜령 <jhr3681@naver.com>
2022-05-21 22:17:34 +0900

    css) movie detail page: simple settings

--- final-pjt-front/src/components/CommentForm.vue
+++ final-pjt-front/src/components/CommentForm.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <h1>CommentForm</h1>
+    <label for="voteRange" class="form-label">Vote range</label>
+    <input type="range" class="form-range" min="0" max="5" step="0.5" id="voteRange">
+    <div class="mb-3">
+      <label for="CommentArea" class="form-label">Comment</label>
+      <textarea class="form-control" id="CommentArea" rows="3"></textarea>
+    </div>
+
   </div>
 </template>
 
--- final-pjt-front/src/views/movies/MovieView.vue
+++ final-pjt-front/src/views/movies/MovieView.vue
@@ -1,13 +1,43 @@
 <template>
   <div>
-    <h1>Movie View</h1>
+    <!-- 영화 포스터란 -->
+    <div class="card mb-3">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="https://movie-phinf.pstatic.net/20220516_144/1652687286550mcE4G_JPEG/movie_image.jpg?type=m203_290_2" class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">Example heading <span class="badge bg-secondary">평점</span></h5>
+            <p class="card-text">개봉일: </p>
+            <p class="card-text">감독: </p>
+            <p class="card-text">배우: </p>
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, iste laboriosam sint, nobis eaque atque veniam accusamus itaque expedita dignissimos eligendi cupiditate velit magnam dolor veritatis accusantium aliquid hic a quos doloremque. Tenetur illo culpa provident assumenda, consectetur accusamus nihil praesentium? Voluptatibus tempora quia quibusdam odio necessitatibus dolore commodi aut.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <hr>
+
+    <!-- 댓글란 -->
+    <comment-list />
+    
   </div>
+
+
 </template>
 
 <script>
-export default {
-  name: 'MovieView.vue',
-}
+  import CommentList from '@/components/CommentList.vue'
+
+
+  export default {
+    name: 'MovieView.vue',
+    components: {
+      CommentList
+    }
+  }
 </script>
 
 <style>
