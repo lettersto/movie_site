d2d976b67f32454d106c3495b840d94dad51bafe
정혜령 <jhr3681@naver.com>
2022-05-22 00:32:12 +0900

    css) community simple settings

---
+++ final-pjt-front/src/components/community/ArticleList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <article-list-item
+      v-for="article in articles" :key="article.pk"
+      :article=article
+    />
+  </div>
+</template>
+
+<script>
+  import ArticleListItem from './ArticleListItem.vue'
+
+  export default {
+    name: 'ArticleList',
+    components: {
+      ArticleListItem
+    },
+    props: {
+      articles: Array
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ final-pjt-front/src/components/community/ArticleListItem.vue
@@ -0,0 +1,60 @@
+<template>
+  <!-- <h1>Community</h1>
+    <router-link 
+      :to="{ name: 'articleNew'}">new
+    </router-link>
+    <ul>
+      <li v-for="article in articles" :key="article.pk">
+        {{ article.user.username }} : 
+        <router-link 
+          :to="{ name: 'articleDetail', params: {articlePk: article.pk} }">
+          {{ article.title }}
+        </router-link>
+        =>
+        ({{ article.comment_count }}) | +{{ article.like_count }}
+      </li>
+    </ul> -->
+  <router-link
+    :to="{ name: 'articleDetail', params: {articlePk: article.pk} }"
+    class="list-group-item list-group-item-action" aria-current="true"
+  >
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1">{{ article.title }}</h5>
+      <div>
+        <small>
+          <span class="material-icons">
+            visibility
+          </span> 30
+        </small> |
+        <small>
+          <span class="material-icons">
+            favorite
+          </span> {{ article.like_count }}
+        </small> |
+        <small>
+          <span class="material-icons">
+            chat
+          </span> {{ article.comment_count }}
+        </small>
+      </div>
+    </div>
+    <p class="mb-1">Some placeholder content in a paragraph.</p>
+    <div class="d-flex justify-content-between">
+      <small>작성자</small>
+      <small>3 days ago</small>
+    </div>
+  </router-link>
+</template>
+
+<script>
+  export default {
+    name: 'ArticleListItem',
+    props: {
+      article: Object
+    }
+  }
+</script>
+
+<style>
+
+</style>
