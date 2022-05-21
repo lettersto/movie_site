336e93693fe023ce1f235d1c6e753807dad7b367
정혜령 <jhr3681@naver.com>
2022-05-21 18:42:45 +0900

    carousel......

--- final-pjt-front/src/components/NavBar.vue
+++ final-pjt-front/src/components/NavBar.vue
@@ -1,11 +1,55 @@
 <template>
   <nav>
     <div class="container">
+      <div class="logo">
+        <i class="material-icons">movie_filter</i>
+        <h1>Cat's Movie</h1>
+      </div>
+
+      <ul class="nav-list">
+        <li>
+          <router-link :to="{ name: 'home' }" class="nav-item mr">
+            Home
+          </router-link>
+        </li>
+        <li>
+          <router-link :to="{ name: 'community' }" class="nav-item">
+            Community
+          </router-link>
+        </li>
+        <li>
+          <search-bar />
+        </li>
+        <li>
+          <router-link v-if="!isLoggedIn" :to="{ name: 'signup' }" class="nav-item mr">
+            Signup
+          </router-link>
+        </li>
+        <li>
+          <router-link v-if="!isLoggedIn" :to="{ name: 'login' }" class="nav-item">
+            Login
+          </router-link>
+        </li>
+        <li>
+          <router-link v-if="isLoggedIn" :to="{ name: 'profile', params: { username: currentUser.username }}" class="nav-item mr">
+            Profile
+          </router-link>            
+        </li>
+        <li>
+          <router-link v-if="isLoggedIn" :to="{ name: 'logout' }" class="nav-item">
+            Logout
+          </router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <!-- <nav>
+    <div class="container">
       <div class="nav-items-left">
-        <!-- <div >
+        <div class="logo">
           <i class="material-icons">movie_filter</i>
-          <span class="logo"> Cat's Movie</span>
-        </div> -->
+          <h1>Cat's Movie</h1>
+        </div>
     
         <div class="nav-items-1">
           <router-link :to="{ name: 'home' }" class="nav-item mr">
@@ -34,7 +78,7 @@
         </router-link>
       </div>
     </div>
-  </nav>
+  </nav> -->
 </template>
 
 <script>
@@ -57,7 +101,11 @@ export default {
 
 <style scoped>
 
-  .container {
+  nav {
+    font-family: 'Poppins', sans-serif;
+  }
+
+  /* .container {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
@@ -69,7 +117,7 @@ export default {
     font-weight: bold;
   } */
 
-  .nav-items-left {
+  /* .nav-items-left {
     display: flex;
     flex-direction: row;
   }
@@ -77,8 +125,7 @@ export default {
 
   .nav-item {
     text-decoration: none;
-    /* color: white; */
-    color: black;
+    color: white;
     font-size: 1.55em;
     font-weight: bold;
   }
@@ -89,7 +136,7 @@ export default {
 
   .mr {
     margin-right: 10px;
-  }
+  }  */
 
   /* .nav-items-1 {
     padding: 0 .8em;
--- final-pjt-front/src/components/homes/EventCarossel.vue
+++ final-pjt-front/src/components/homes/EventCarossel.vue
@@ -1,15 +1,81 @@
 <template>
-  <div>
-    <h1>Event Carossel</h1>
+    
+  <div class="carousel">
+    <div class="carousel-inner">
+      <carousel-control @prev="prev" @next="next"/>
+      <carousel-items 
+        v-for="(slide, index) in slides" :key="`item-${index}`"
+        :slide="slide" :currentslide="currentslide" :index="index"
+        :direction="direction"
+      />
+    </div>
   </div>
+
 </template>
 
 <script>
+import CarouselItems from '@/components/homes/CarouselItems.vue'
+import CarouselControl from '@/components/homes/CarouselControl.vue'
+
+
 export default {
   name: 'EventCarossel',
+  components: {
+    CarouselItems,
+    CarouselControl
+  },
+  data() {
+    return {
+        currentslide: 0,
+        slideinterval: null,
+        direction: "right",
+        slides: [
+          "https://images.unsplash.com/photo-1585951237318-9ea5e175b891?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032",
+          "https://images.unsplash.com/photo-1585951237318-9ea5e175b891?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032",
+          "https://images.unsplash.com/photo-1552417559-f62e53cba705?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032"
+        ],
+    }
+  },
+  methods: {
+    setCurrentSlide(index) {
+      this.currentslide = index; 
+    },
+    prev() {
+      const index = this.currentslide > 0 ? this.currentslide - 1 : this.slides.length-1;
+      this.setCurrentSlide(index);
+      this.direction = "left";
+    },
+    next() {
+      const index = this.currentslide < this.slides.length - 1 ? this.currentslide + 1 : 0;
+      this.setCurrentSlide(index);
+      this.direction = "right";
+    }
+  },
+  mounted() {
+    this.slideinterval = setInterval(() => {
+      const index = this.currentslide < this.slides.length - 1 ? this.currentslide + 1 : 0;
+      this.setCurrentSlide(index);
+    }, 3000)
+  },
+
+  beforeUnmount() {
+    clearInterval(this.slideInterval);
+  }
 }
 </script>
 
-<style>
+<style scoped>
+
+  .carousel {
+    display: flex;
+    justify-content: center;
+  }
+
+  .carousel-inner {
+    position: relative;
+    width: 900px;
+    height: 400px;
+    overflow: hidden;
+  }
 
 </style>
