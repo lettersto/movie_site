2d9b54b531a746148294874c8ff0282817e1b0f5
정혜령 <jhr3681@naver.com>
2022-05-22 02:25:35 +0900

    add heart to like button and correction mark

--- final-pjt-front/src/views/community/ArticleDetailView.vue
+++ final-pjt-front/src/views/community/ArticleDetailView.vue
@@ -1,24 +1,36 @@
 <template>
   <div>
-    <h1>{{ article.title }}</h1>
-    <p>
-      {{ article.content }}
-    </p>
-    <div v-if="isAuthor">
-      <router-link :to="{ name: 'articleEdit', params: { articlePk } }">
-        <button>Edit</button>
-      </router-link>
-      |
-      <button @click="deleteArticle(articlePk)">Delete</button>
-    </div>
-    <div>
-      Likeit:
-      <button
-        @click="likeArticle(articlePk)"
-      >{{ likeCount }}</button>
-    </div>
+    <!-- 게시글 -->
+    <article>
+      <div class="d-flex align-items-center">
+        <h1>{{ article.title }}</h1>
+        <small v-show="isCorreted">(수정)</small>
+      </div>
+      <p>
+        {{ article.content }}
+      </p>
+      <!-- 수정, 삭제 버튼 -->
+      <div v-if="isAuthor">
+        <router-link :to="{ name: 'articleEdit', params: { articlePk } }">
+          <button>Edit</button>
+        </router-link>
+        |
+        <button @click="deleteArticle(articlePk)">Delete</button>
+      </div>
+      <!-- 좋아요 버튼 -->
+      <div>
+        <button @click="likeArticle(articlePk)">
+          <i v-show="inLikeArticleList" class="material-icons like">favorite</i>
+          <i v-show="!inLikeArticleList" class="material-icons no-like">heart_broken</i>
+        </button> {{ likeCount }}
+      </div>
+    </article>
+    
     <hr />
+    
+    <!-- Comment section -->
     <comment-list :comments="article.comments"></comment-list>
+
   </div>
 </template>
 
@@ -35,9 +47,20 @@
       }
     },
     computed: {
-      ...mapGetters(['isAuthor', 'article']),
+      ...mapGetters(['isAuthor', 'article', 'currentUser']),
       likeCount() {
         return this.article.like_users?.length
+      },
+      inLikeArticleList() { // 미리 계산하고 와야할 거 같은데 임시로
+        let inLike = false;
+        this.article.user_like.forEach(user => {
+          if (user.pk === this.currentUser.pk) inLike = true;
+        });
+        return inLike;
+      },
+      isCorreted() {
+        if (this.article.created_at !== this.article.updated_at) return true;
+        else return false;
       }
     },
     methods: {
@@ -53,5 +76,19 @@
   }
 </script>
 
-<style>
+<style scoped>
+  button {
+    background: none;
+    border: none;
+    outline: none;
+  }
+
+  .like {
+    color: #F02D3A;
+  }
+
+  .no-like {
+    color: #273043;
+  }
+
 </style>
