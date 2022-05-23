d7242d06be0c0ba5fbb37ee6acff43aa00be98f6
정혜령 <jhr3681@naver.com>
2022-05-23 15:43:52 +0900

    article search bar!

---
+++ final-pjt-front/src/components/community/ArticleSearchedItems.vue
@@ -0,0 +1,29 @@
+<template>
+  <li @click="onClick(article.pk)" class="article-searched-list-items">
+    {{ article.title }}
+  </li>
+</template>
+
+<script>
+  export default {
+    name: 'ArticleSearchedItems',
+    props: {
+      article: Object
+    },
+    methods: {
+      onClick(articleId) {
+        this.$router.push({ name: 'articleDetail', params: { articlePk: articleId}})
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .article-searched-list-items {
+    list-style: none;
+    cursor: pointer;
+  }
+  .article-searched-list-items:hover {
+    font-weight: bold;
+  }
+</style>
